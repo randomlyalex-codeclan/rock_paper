2cbad23c3c8b12435fc7499e9f6b9d1b1a1c7289
Alex <alexdodd@gmail.com>
2020-12-05 16:59:51 +0000

    take 2 started right over, this is the a hello world flask base/index.html init setup

---
+++ app/controller.py
@@ -0,0 +1,28 @@
+from flask import render_template, request, redirect
+from app import app
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Home')
+
+# @app.route('/add-event', methods=['POST'])
+# def add_event():
+#   date = request.form['date']
+#   # Split the date into a list
+#   split_date = date.split('-')
+#   # create a new date object
+#   date = datetime.date(int(split_date[0]), int(split_date[1]), int(split_date[2]))
+#   name = request.form['name']
+#   guests = request.form['guests']
+#   recurring = True if 'recurring' in request.form else False
+#   roomLocation = request.form['roomLocation']
+#   description = request.form['description']
+#   newevent = Event(date=date, name= name, guests=guests, recurring=recurring, room_location=roomLocation, description=description)
+#   add_new_event(newevent)
+#   return redirect('/')
+
+# @app.route('/delete/<name>', methods=['POST'])
+# def delete(name):
+#   delete_event(name)
+#   return redirect('/')
