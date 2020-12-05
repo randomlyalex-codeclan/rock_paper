dba3c0dac29553444468df4c3c7acca5ca86763b
Alex <alexdodd@gmail.com>
2020-12-05 17:47:20 +0000

    created a Player Class and Rock_Paper class, call them and return things

--- app/controller.py
+++ app/controller.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, redirect
 from app import app
+from app.models.rock_paper import RockPaper
+from app.models.player import Player
 
 
 @app.route('/')
@@ -7,9 +9,14 @@ def index():
     return render_template('index.html', title='Home')
 
 
-@app.route('/basic/<player_1>/<player_2>')
-def basic(player_1, player_2):
-    return f"{player_1} vs {player_2}"
+@app.route('/basic/<player_1_choice>/<player_2_choice>')
+def basic(player_1_choice, player_2_choice):
+    player_1 = Player("Player 1", player_1_choice)
+    player_2 = Player("Player 2", player_2_choice)
+    game = RockPaper(player_1, player_2)
+    # do the thing here in the models
+    # then return the thing from the models to the viewer ie templates page.
+    return f"Winner is: {game.check_winner(player_1, player_2).name}"
 
 # @app.route('/add-event', methods=['POST'])
 # def add_event():
---
+++ app/models/rock_paper.py
@@ -0,0 +1,8 @@
+class RockPaper():
+
+    def __init__(self, player_1, player_2):
+        self.player_1 = player_1
+        self.player_2 = player_2
+
+    def check_winner(self, player_1, player_2):
+        return player_1
